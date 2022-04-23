b286ca2cb89f448fbeeeb82c8d0020070e279a79
cquentin26200 <qcheronpedenschool.fr>
2022-04-23 18:31:51 +0200

    add search bar

--- js/script.js
+++ js/script.js
@@ -2,13 +2,32 @@ const secondNavHeader = document.querySelector(".secondNavHeader");
 const planetPossible = document.querySelector(".planet-possible");
 
 document.addEventListener("scroll", () => {
-    if (window.scrollY >= planetPossible.offsetTop) {
-        secondNavHeader.style.position = "fixed";
-        secondNavHeader.style.top = "0";
-        secondNavHeader.style.transition = "all .50s ease";
-    } else {
-        secondNavHeader.style.top = "-10rem";
-        secondNavHeader.style.position = "static";
-    }
-})
+  if (window.scrollY >= planetPossible.offsetTop) {
+    secondNavHeader.style.position = "fixed";
+    secondNavHeader.style.top = "0";
+    secondNavHeader.style.transition = "all .50s ease";
+  } else {
+    secondNavHeader.style.top = "-10rem";
+    secondNavHeader.style.position = "static";
+  }
+});
 
+const activeSearchBar = document.querySelector(".activeSearchBar");
+const searchBar = document.querySelector(".searchBar");
+const divSearchBar = document.querySelector(".searchBar div");
+
+searchBar.style.opacity = "0";
+searchBar.style.zIndex = "-1";
+
+activeSearchBar.addEventListener("click", () => {
+  searchBar.style.position = "fixed";
+  searchBar.style.opacity = "1";
+  searchBar.style.zIndex = "100";
+});
+
+const crossArrow = document.querySelector(".crossMenuHeader");
+
+crossArrow.addEventListener("click", () => {
+  searchBar.style.opacity = "0";
+  searchBar.style.zIndex = "-1";
+});
